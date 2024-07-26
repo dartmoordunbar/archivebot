ed3f80e0eddeb6bac82e78bbe8bc8092edd7d301
Jamie Dunbar <53050570+dartmoordunbar@users.noreply.github.com>
2024-07-26 12:14:17 +0100

    Update index.py

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id= os.environ["aws_access_key_id"]
+    aws_access_key_id= os.environ["aws_access_key_id"],
     aws_secret_access_key=os.environ["aws_secret_access_key"]
 )
 input_root = "/data/collections"
